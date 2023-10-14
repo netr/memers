b3b3da71fb63522081498684ee6ab3f3fcd742d0
netr <programmatical@gmail.com>
2023-10-13 22:04:31 -0700

    change Gas fields to be U256 instead of u64 for consistency

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 use anyhow::{anyhow, Result};
 use chrono::{DateTime, Utc};
+use ethers_core::types::U256;
 use ethers_core::{types::H256, utils::format_units};
 use ethers_providers::{Http, Middleware, Provider};
 use fern::colors::{Color, ColoredLevelConfig};
@@ -49,17 +50,17 @@ pub fn convert_utc_to_local_str(timestamp: u64) -> String {
 #[derive(Debug, Clone)]
 pub struct Gas {
     /// The gas price (gas_price)
-    gas_price: u64,
+    gas_price: U256,
     /// The base fee derived from the [`ethers::types::Block`]
-    base_fee: u64,
+    base_fee: U256,
     /// The maximum fee known as `gas_tip_cap` in EVM
-    max_fee: u64,
+    max_fee: U256,
     /// The maximum priority fee known as `gas_fee_cap` in EVM
-    max_prio_fee: u64,
+    max_prio_fee: U256,
     /// The amount of gas used from the [`ethers::types::TransactionReceipt`]
-    gas_used: u64,
+    gas_used: U256,
     /// The total cost of the transaction in wei
-    tx_cost: u64,
+    tx_cost: U256,
 }
 
 impl Gas {
@@ -69,7 +70,7 @@ impl Gas {
     /// * `gas_used` - The amount of gas used from the [`ethers::types::TransactionReceipt`]
     /// * `max_fee` - The maximum fee known as `gas_tip_cap` in EVM
     /// * `max_prio_fee` - The maximum priority fee known as `gas_fee_cap` in EVM
-    pub fn new(gas_used: u64, base_fee: u64, max_fee: u64, max_prio_fee: u64) -> Self {
+    pub fn new(gas_used: U256, base_fee: U256, max_fee: U256, max_prio_fee: U256) -> Self {
         let mut gas_price = base_fee.add(max_prio_fee);
         let mut tx_cost = (base_fee + max_prio_fee) * gas_used;
         if max_fee == max_prio_fee {
@@ -134,17 +135,17 @@ impl Gas {
         };
 
         let gas = Gas::new(
-            receipt.gas_used.unwrap().as_u64(),
-            block.base_fee_per_gas.unwrap().as_u64(),
-            tx.max_fee_per_gas.unwrap().as_u64(),
-            tx.max_priority_fee_per_gas.unwrap().as_u64(),
+            receipt.gas_used.unwrap(),
+            block.base_fee_per_gas.unwrap(),
+            tx.max_fee_per_gas.unwrap(),
+            tx.max_priority_fee_per_gas.unwrap(),
         );
 
         Ok(gas)
     }
 
     /// Returns the gas price in wei
-    pub fn gas_price(&self) -> u64 {
+    pub fn gas_price(&self) -> U256 {
         self.gas_price
     }
 
@@ -154,7 +155,7 @@ impl Gas {
     }
 
     /// Returns the base fee in wei
-    pub fn base_fee(&self) -> u64 {
+    pub fn base_fee(&self) -> U256 {
         self.base_fee
     }
 
@@ -164,7 +165,7 @@ impl Gas {
     }
 
     /// Returns the maximum fee in wei
-    pub fn max_fee(&self) -> u64 {
+    pub fn max_fee(&self) -> U256 {
         self.max_fee
     }
 
@@ -174,7 +175,7 @@ impl Gas {
     }
 
     /// Returns the maximum priority fee in wei
-    pub fn max_priority_fee(&self) -> u64 {
+    pub fn max_priority_fee(&self) -> U256 {
         self.max_prio_fee
     }
 
@@ -184,12 +185,12 @@ impl Gas {
     }
 
     /// Returns the amount of gas used in wei
-    pub fn gas_used(&self) -> u64 {
+    pub fn gas_used(&self) -> U256 {
         self.gas_used
     }
 
     /// Returns the total cost of the transaction in wei
-    pub fn tx_cost(&self) -> u64 {
+    pub fn tx_cost(&self) -> U256 {
         self.tx_cost
     }
 
@@ -219,7 +220,9 @@ mod tests {
     use super::*;
 
     #[test]
+    #[ignore]
     fn it_should_convert_utc_to_local_str() {
+        // Ignored because this test will fail if you're not in the same time zone as me.
         // Your time zone: Wednesday, August 25, 2021 7:00:00 AM GMT-07:00 DST
         let timestamp = 1629900000;
         let expected = "2021-08-25 07:00:00.000000000";
@@ -241,10 +244,10 @@ mod tests {
         // Is this the right way to test this? I'm not sure. Is it even worth testing? Seems redundant in a way.
         // This tests the underlying *_converted() functions, but not the Display trait itself.
         // If anything, this can be used for documentation purposes.
-        let base_fee = 8681938922;
-        let gas_used = 182197;
-        let max_fee = 9121299698;
-        let max_prio_fee = 50000000;
+        let base_fee = U256::from(8681938922u64);
+        let gas_used = U256::from(182197u64);
+        let max_fee = U256::from(9121299698u64);
+        let max_prio_fee = U256::from(50000000u64);
 
         let expected_display = "price: 8.731938922 gwei, base fee: 8.681938922 gwei, max fee: 9.121299698 gwei, max prio fee: 0.050000000 gwei, gas used: 182197 wei, tx cost: 0.001590933075771634 eth";
         let expected_debug = "Gas { gas_price: 8731938922, base_fee: 8681938922, max_fee: 9121299698, max_prio_fee: 50000000, gas_used: 182197, tx_cost: 1590933075771634 }";
@@ -257,10 +260,10 @@ mod tests {
 
     #[test]
     fn it_should_calculate_the_correct_transaction_cost() {
-        let base_fee = 8681938922;
-        let gas_used = 182197;
-        let max_fee = 9121299698;
-        let max_prio_fee = 50000000;
+        let base_fee = U256::from(8681938922u64);
+        let gas_used = U256::from(182197u64);
+        let max_fee = U256::from(9121299698u64);
+        let max_prio_fee = U256::from(50000000u64);
 
         let expected = Gas {
             gas_price: base_fee + max_prio_fee,
@@ -268,7 +271,7 @@ mod tests {
             max_fee,
             max_prio_fee,
             gas_used,
-            tx_cost: 1590933075771634,
+            tx_cost: U256::from(1590933075771634u64),
         };
 
         let actual = Gas::new(gas_used, base_fee, max_fee, max_prio_fee);
@@ -282,10 +285,10 @@ mod tests {
 
     #[test]
     fn it_should_calculate_the_correct_transaction_cost_when_max_fee_equals_max_priority_fee() {
-        let base_fee = 8681938922;
-        let gas_used = 182197;
-        let max_fee = 50000000;
-        let max_prio_fee = 50000000;
+        let base_fee = U256::from(8681938922u64);
+        let gas_used = U256::from(182197u64);
+        let max_fee = U256::from(50000000u64);
+        let max_prio_fee = U256::from(50000000u64);
 
         let expected = Gas {
             gas_price: max_fee,
@@ -293,7 +296,7 @@ mod tests {
             max_fee,
             max_prio_fee,
             gas_used,
-            tx_cost: 9109850000000,
+            tx_cost: U256::from(9109850000000u64),
         };
 
         let actual = Gas::new(gas_used, base_fee, max_fee, max_prio_fee);
@@ -306,10 +309,10 @@ mod tests {
 
     #[test]
     fn it_should_calculate_the_correct_transaction_cost_when_max_fee_equals_gas_price() {
-        let base_fee = 8681938922;
-        let gas_used = 182197;
-        let max_fee = 8681938922;
-        let max_prio_fee = 50000000;
+        let base_fee = U256::from(8681938922u64);
+        let gas_used = U256::from(182197u64);
+        let max_fee = U256::from(8681938922u64);
+        let max_prio_fee = U256::from(50000000u64);
 
         let expected = Gas {
             gas_price: base_fee + max_prio_fee,
@@ -317,7 +320,7 @@ mod tests {
             max_fee,
             max_prio_fee,
             gas_used,
-            tx_cost: 1590933075771634,
+            tx_cost: U256::from(1590933075771634u64),
         };
 
         let actual = Gas::new(gas_used, base_fee, max_fee, max_prio_fee);
